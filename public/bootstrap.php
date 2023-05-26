79b563ee1382b17cde7f0d39a1549eb082ba6086
Sven Schrodt <sven@schrodt.club>
2023-05-26 10:29:58 +0200

    Adding some frontend stuff

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -11,17 +11,3 @@ declare(strict_types=1);
 
 
 require_once 'private/lib/Html5_Lab/Autoload.php';
-use SchrodtSven\Html5_Lab\App\Config;
-use SchrodtSven\Html5_Lab\Kernel\StringType;
-use SchrodtSven\Html5_Lab\Kernel\ListType;
-use SchrodtSven\Html5_Lab\Kernel\Frontend\HtmlElement;
-
-$tmp = ListType::createFromFile('archive/all_attributes.txt');
-foreach($tmp as $item) {
-
-    $t = (new StringType($item))->splitBy("\t");
-    echo $t[1];
-    echo PHP_EOL;
-
-  //  die();
-}
